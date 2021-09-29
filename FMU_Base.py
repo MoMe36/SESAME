a100b460041fa6d0dd5fd24b7c31147ba662d5e1
Mehdi Mounsif <mounsif.mehdi@gmail.com>
2021-09-29 10:39:50 +0200

    FMU_pipe to test

--- FMU_Base.py
+++ FMU_Base.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as N
 
 # Load the FMU
-model = load_fmu('RLC_FMU.fmu')
+# model = load_fmu('RLC_FMU.fmu')
 
  # Generate input
 t = N.linspace(0.,10.,10000) 
 u = N.full_like(t,0.5)                  # Constante de la forme de t
 u_traj = N.transpose(N.vstack((t,u)))
- 
+print(u_traj.shape)
+
 # Create input object
 input_object = ('u', u_traj)
     
